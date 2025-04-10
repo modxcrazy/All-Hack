6f06cf6c20813e7780e3c68de6631f14efe09932
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-11 00:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,9 @@ function showPopup(event) {
   const popup = document.getElementById("popupMenu");
   popup.classList.remove("hidden");
 
-  // Position popup near the clicked share icon
   popup.style.left = event.pageX + "px";
   popup.style.top = event.pageY + "px";
 
-  // Hide on outside click
   document.addEventListener("click", function handler(e) {
     if (!popup.contains(e.target)) {
       popup.classList.add("hidden");
