1ed01387c338e9f5e1af0384351f5c6d45b67f57
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-11 00:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function goTo(index) {
   ];
   window.location.href = urls[index - 1];
 }
+
+function showPopup(event) {
+  const popup = document.getElementById("popupMenu");
+  popup.classList.remove("hidden");
+
+  // Position popup near the clicked share icon
+  popup.style.left = event.pageX + "px";
+  popup.style.top = event.pageY + "px";
+
+  // Hide on outside click
+  document.addEventListener("click", function handler(e) {
+    if (!popup.contains(e.target)) {
+      popup.classList.add("hidden");
+      document.removeEventListener("click", handler);
+    }
+  });
+}
