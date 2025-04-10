6fc207391a485b8b28db59df3d664617283fe67b
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-10 23:58:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function goTo(index) {
+  const urls = [
+    "https://modxcrazy.github.io/buy/",
+    "https://modxcrazy.github.io/buy/",
+    "https://modxcrazy.github.io/buy/",
+    "https://modxcrazy.github.io/buy/",
+    "https://modxcrazy.github.io/buy/"
+  ];
+  window.location.href = urls[index - 1];
+}
